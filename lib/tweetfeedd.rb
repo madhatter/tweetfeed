a8f711422628fe92bbaa50466296cdfd6bb33726
Arvid Warnecke <madhatter@nostalgix.org>
2012-02-09 22:38:22 +0100

    Basic tweetfeedd daemon class.

---
+++ lib/tweetfeedd.rb
@@ -0,0 +1,24 @@
+require 'daemons'
+require 'eventmachine'
+require 'logger'
+
+class Tweetfeedd
+  def run
+    Daemons.run_proc('tweetfeedd', :dir_mode => :script, :dir => './', \
+                     :backtrace => true, :log_output => true) do
+      @logger = Logger.new(STDOUT)
+      @logger.info "Starting tweetfeed daemon..."
+
+      EventMachine::run {
+        EventMachine::add_periodic_timer(20) {
+          @logger.info "Searching..."
+          begin
+            puts "Here we'll call the tweedfeed class later..."
+          rescue InternalServerError
+            $stderr.puts "Something went terribly wrong."
+          end
+        }
+      }
+    end
+  end
+end
