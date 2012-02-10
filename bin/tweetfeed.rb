5eb2b017f5fd9b8bfafabeead0653c331ad81b47
Arvid Warnecke <madhatter@nostalgix.org>
2012-02-10 18:13:15 +0100

    Initial configuration class

--- bin/tweetfeed.rb
+++ bin/tweetfeed.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 
 require_relative '../lib/tweetfeedd.rb'
+require_relative '../lib/tweetfeed_config.rb'
 
 begin
-  daemon = Tweetfeedd.new()
-  daemon.run 
+  config = Tweetfeed_config.new
+
+  #daemon = Tweetfeedd.new
+  #daemon.run 
 
 end
 
--- lib/tweetfeed_config.rb
+++ lib/tweetfeed_config.rb
@@ -1,11 +1,18 @@
 require 'yaml'
+require 'logger'
 
 # Class for handling the configuration file
 # of tweetfeed
 
 class Tweetfeed_config
-  def read
-    nil
+  def initialize
+    @CONFIG_FILE = 'tweetfeed.yml'
+    @logger = Logger.new(STDOUT)
+    config_file = File.join(Dir.pwd, 'config', @CONFIG_FILE)
+    @configuration = YAML.load_file(config_file)
+    
+    puts "Heh?!"
+    @logger.info "Config file read..."
   end
 end
 
