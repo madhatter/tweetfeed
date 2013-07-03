0dce88dacc79bd22644a9c15bc0db67075632200
Arvid Warnecke <madhatter@nostalgix.org>
2013-07-03 17:31:08 +0200

    Fixing the search method to collect all results and not only the last one.

--- lib/tweetfeed.rb
+++ lib/tweetfeed.rb
@@ -26,7 +26,7 @@ class Tweetfeed
   # Starts the search and generates the RSS feed file.
   def run
     @logger.debug "LastId is " + @last_id.to_s
-    tweets = search
+    tweets = collect_tweets
     
     @generator.generate_rss_file tweets unless tweets.instance_of? NilClass
     
@@ -36,51 +36,63 @@ class Tweetfeed
   end
 
   # Search for hashtags at Twitter
-  def search
+  def collect_tweets
     result = []
     begin
       @hashtags.each do |hashtag|
-        result = @twitter.search("##{hashtag} -rt", :since_id => @last_id, :include_entities => 1).statuses
+        result.push search(hashtag)
       end
 
       # for debugging TODO remove later
-#      result.statuses.each do |status|
-#        puts status.text
-#      end
-#      exit
+      #result.pop.each do |status|
+      #  puts status.id
+      #end
+      #exit
+
       store_last_id calculate_last_id result
       filter_tweets_with_urls result
 
-    rescue EOFError, SocketError
+    rescue EOFError, SocketError, Twitter::Error::ClientError
       @logger.error "Connection to Twitter seems to be not available."
+      exit
     end
   end
 
+  def search hashtag
+    result = @twitter.search("##{hashtag} -rt", :since_id => @last_id, :include_entities => 1).statuses
+  end
+
   # Get the max tweet id from the last search result
-  def calculate_last_id tweets
+  def calculate_last_id tweets_array
     last_id = @last_id
-    tweets.each do |t| 
-      last_id = t['id'] if t['id'] > last_id 
+    tweets_array.each do |tweets| 
+      tweets.each do |t| 
+        last_id = t['id'] if t['id'] > last_id 
+      end
     end
     last_id
   end
 
   # Store the tweet id from the latest search in the configuration
   def store_last_id last_id
+    @logger.info "Storing the last id to the configuration."
     @config.last_id = last_id
     @last_id = @config.last_id
   end
 
   # Filter the tweets for tweets with URLs only.
-  def filter_tweets_with_urls tweets
+  def filter_tweets_with_urls tweets_array
     tweets_with_urls = []
-    tweets.each do |tweet|
-      # TODO remove this, just for debugging
-#      tweet.urls.each do |url| 
-#        p url.display_url
-#      end
-      tweets_with_urls << tweet unless tweet.urls.empty?
+    tweets_array.each do |tweets|
+      tweets.each do |tweet|
+        # TODO remove this, just for debugging
+#       tweet.urls.each do |url| 
+#         p url.display_url
+#       end
+       tweets_with_urls << tweet unless tweet.urls.empty?
+      end
     end
+    p tweets_with_urls
     tweets_with_urls
   end
 end
