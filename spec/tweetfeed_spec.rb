d32774c92f4885344b796d44c43e6c0fe20c274d
Arvid Warnecke <madhatter@nostalgix.org>
2013-07-04 07:26:41 +0200

    Added new tests for the tweetfeed class

---
+++ spec/tweetfeed_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper.rb'
+require 'yaml'
+require_relative '../lib/tweetfeed_config.rb'
+require_relative '../lib/tweetfeed.rb'
+
+describe Tweetfeed do
+  before :each do
+    logger = double(:logger, :info => nil, :level= => nil, :error => nil)
+    @tweetfeed_conf = TweetfeedConfig.new logger
+    config_file = File.join(Dir.pwd, 'spec', 'test_config.yml')
+    @tweetfeed_conf.read config_file
+    @tweetfeed = Tweetfeed.new(@tweetfeed_conf)
+
+    tweets_file = File.join(Dir.pwd, 'spec', 'tweets.yml')
+    @tweets_array = YAML.load_file(tweets_file)
+  end
+
+  it "should have some configurations set" do
+    config = @tweetfeed_conf
+    response = config.log_level
+    response.should == 3
+  end
+
+  it "should find x tweets with urls" do
+    tf = @tweetfeed
+    response = tf.filter_tweets_with_urls @tweets_array
+    response.size.should == 3
+  end
+end
+
