05bd3665d1646552f991155c8d5b49173866929f
Arvid Warnecke <madhatter@nostalgix.org>
2016-03-08 23:05:32 +0100

    Working url parser for https urls.

--- lib/tweetfeed_url_parser.rb
+++ lib/tweetfeed_url_parser.rb
@@ -9,13 +9,33 @@ class UrlParser
   end
 
   def get_original_url url
-    redirect_url = url
+    redirect_url = nil
     @logger.debug "Fetching original url for #{url}"
 
-    while url == redirect_url do
-      res = Net::HTTP.get_response(URI(url.gsub('https', 'http')))
-      redirect_url = res['location']
+    while url != redirect_url do
+      @logger.debug "Looking..."
+      uri = URI.parse(url)
+      if uri.scheme == 'https'
+        uri.port = Net::HTTP.https_default_port()
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE # read into this
+
+        response = http.get2(uri)
+      else
+        response = Net::HTTP.get_response(uri)
+      end
+
+      redirect_url = response['location']
+      if redirect_url.nil?
+        redirect_url = url
+      end
+
+      @logger.debug "Found #{redirect_url}"
+      @logger.debug "For   #{url}"
+      url = redirect_url
     end
-    redirect_url
+      redirect_url
   end
 end
